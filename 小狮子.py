7d423d9014e6708135ebd39efb4c4033f26b8452
zhuqian <i@zhuqian.xyz>
2021-05-27 14:58:05 +0800

    加了注释和缺的图片，调整图像位置

--- 小狮子.py
+++ 小狮子.py
@@ -7,15 +7,16 @@ import tkinter as tk
 from PIL import Image, ImageTk, ImageSequence
 import time
 from threading import Thread
-#flag为1时，小狮子说话
+#flag为1时，小狮子嘴巴动起来
 flag=1
-#语音路径
+#语音路径 自定义
 path=r'C:\Users\zhuqian\Desktop\s\myword.mp3'
 def asyn(f):
     def wrapper(*args, **kwargs):
         thr = Thread(target = f, args = args, kwargs = kwargs)
         thr.start()
     return wrapper
+#录音时开启一个线程防止卡死
 @asyn
 def chatting():
     
@@ -51,26 +52,28 @@ def chatting():
     time.sleep(2)
 
 
-def bizui():
-    global image,pic,canvas,root,flag
-    flag=0
-    image = Image.open("2.jpg")  
-    pic=ImageTk.PhotoImage(image)
-    canvas.create_image((800,850), image=pic)
-    root.update_idletasks()  #刷新
-    root.update()
+# def bizui():
+#     global image,pic,canvas,root,flag
+#     flag=0
+#     image = Image.open("2.jpg")
+#     pic=ImageTk.PhotoImage(image)
+#     canvas.create_image((800,850), image=pic)
+#     root.update_idletasks()  #刷新
+#     root.update()
             
 
 #点击说话
-   
+'''
+点击按钮时的操作：
+'''
 def test():
-    B.configure(state=DISABLED)
+    B.configure(state=DISABLED) #按钮变灰色
     global flag
     flag=1
     
-    chatting()
-    pick(1)
-    B.configure(state="normal")
+    chatting()  #开始聊天
+    pick(1)     #图片开始动
+    B.configure(state="normal") #对话结束，按钮恢复
 
 #分解gif并逐帧显示
 
@@ -85,20 +88,20 @@ def pick(event):
         #frame就是gif的每一帧，转换一下格式就能显示了
         for frame in iter:
             pic=ImageTk.PhotoImage(frame)
-            canvas.create_image((800,850), image=pic)
+            canvas.create_image((600,500), image=pic)
             time.sleep(0.1)
             root.update_idletasks()  #刷新
             root.update()
 root = Tk()
-root.geometry("1500x1800")
+root.geometry("1250x1618")
 B = Button(text ="点击说话" ,command=test)
 B.pack()
 
-canvas = Canvas(root,width=1500, height=1800,bg='white')
+canvas = Canvas(root,width=1200, height=1200,bg='white')
 canvas.pack()
 image = Image.open("2.jpg")  
 pic=ImageTk.PhotoImage(image)
-canvas.create_image((800,850), image=pic) 
+canvas.create_image((600,500), image=pic)
 
 
         
